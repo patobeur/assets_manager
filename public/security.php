9ac4145207155814293b2c517a7b182d5fbb4f6e
ProfLambda <fernando.sanchez@imc.randstad.fr>
2025-10-29 10:19:43 +0100

    modified:   public/login.php 	new file:   public/logs/php_errors.log 	modified:   public/security.php

--- public/security.php
+++ public/security.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 // Specify the path to the error log file.
-ini_set('error_log', realpath(dirname(__FILE__)) . '/../logs/php_errors.log');
+ini_set('error_log', __DIR__ . '/../logs/php_errors.log');
 // Report all PHP errors.
 error_reporting(E_ALL);
 
